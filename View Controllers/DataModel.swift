aa8eea32fad4aef9fba4813e71a03c1abade1bdc
Paul Adams <paul@pkadams67.io>
2018-01-28 08:36:51 -0600

    Added onboarding metatdata and imagery.

--- View Controllers/DataModel.swift
+++ View Controllers/DataModel.swift
@@ -19,8 +19,8 @@ public final class DataModel: NSObject, OnboardingViewDelegate, OnboardingViewDa
             return OnboardingConfiguration(
                 image: UIImage(named: "onboard0")!,
                 itemImage: UIImage(named: "number1")!,
-                pageTitle: "Welcome!",
-                pageDescription: "“Observe a blessed Lent this year by self-examination and repentance; by prayer, fasting, and self-denial; and by reading and meditating on God’s Holy Word.”",
+                pageTitle: "Prepare",
+                pageDescription: "Early Christians observed a season of penitence and fasting in preparation for the Paschal feast, or Pascha. The season known now as Lent (from an Old English word meaning “spring,” the time of lengthening days) has a long history. Originally, in places where Pascha was celebrated on a Sunday, the Paschal feast followed a fast of up to two days.",
                 backgroundImage: UIImage(named: "background0"),
                 topBackgroundImage: nil,
                 bottomBackgroundImage: UIImage(named: "backgroundWave")
@@ -30,8 +30,8 @@ public final class DataModel: NSObject, OnboardingViewDelegate, OnboardingViewDa
             return OnboardingConfiguration(
                 image: UIImage(named: "onboard1")!,
                 itemImage: UIImage(named: "number2")!,
-                pageTitle: "Reflection",
-                pageDescription: "Some words about reflection",
+                pageTitle: "Reflect",
+                pageDescription: "In the third century, this fast was lengthened to six days. Eventually, this fast overlapped another fast of forty days, in imitation of Christ’s fasting in the wilderness. The forty-day fast was especially important for converts to the faith who were preparing for baptism, and for those guilty of notorious sins who were being restored to the Christian assembly.",
                 backgroundImage: UIImage(named: "background1"),
                 topBackgroundImage: nil,
                 bottomBackgroundImage: UIImage(named: "backgroundWave")
@@ -41,8 +41,8 @@ public final class DataModel: NSObject, OnboardingViewDelegate, OnboardingViewDa
             return OnboardingConfiguration(
                 image: UIImage(named: "onboard2")!,
                 itemImage: UIImage(named: "number3")!,
-                pageTitle: "Preparation",
-                pageDescription: "Some words about preparation",
+                pageTitle: "Renew",
+                pageDescription: "In the western church, the forty days of Lent extend from Ash Wednesday through Holy Saturday, omitting Sundays. The last three days of Lent are the sacred Triduum of Maundy Thursday, Good Friday, and Holy Saturday. Today Lent has reacquired its significance as the final preparation of adult candidates for baptism.",
                 backgroundImage: UIImage(named: "background2"),
                 topBackgroundImage: nil,
                 bottomBackgroundImage: UIImage(named: "backgroundWave")
@@ -52,8 +52,8 @@ public final class DataModel: NSObject, OnboardingViewDelegate, OnboardingViewDa
             return OnboardingConfiguration(
                 image: UIImage(named: "onboard3")!,
                 itemImage: UIImage(named: "number4")!,
-                pageTitle: "Renewal",
-                pageDescription: "Some words about renewal",
+                pageTitle: "Recommit",
+                pageDescription: "Joining with them, all Christians are invited to the observance of a holy Lent, by self-examination and repentance; by prayer, fasting, and self-denial; and by reading and meditating on God’s holy Word.",
                 backgroundImage: UIImage(named: "background3"),
                 topBackgroundImage: nil,
                 bottomBackgroundImage: UIImage(named: "backgroundWave")
@@ -71,10 +71,9 @@ public final class DataModel: NSObject, OnboardingViewDelegate, OnboardingViewDa
         pageView.titleLabel.layer.shadowOffset       = CGSize(width: 0, height: 1)
         pageView.titleLabel.layer.shouldRasterize    = true
         pageView.titleLabel.layer.rasterizationScale = UIScreen.main.scale
-        pageView.titleLabel.font                     = UIFont.boldSystemFont(ofSize: 30)
-        pageView.descriptionLabel.font               = UIFont.systemFont(ofSize: 20)
+        pageView.titleLabel.font                     = UIFont(name: "HelveticaNeue-Bold", size: 36) ?? UIFont.boldSystemFont(ofSize: 36)
+        pageView.descriptionLabel.font               = UIFont(name: "HelveticaNeue-Thin", size: 17) ?? UIFont.systemFont(ofSize: 17)
     }
-    
     public func onboardingView(_ onboardingView: OnboardingView, didSelectPage page: Int) {
         print("Did select Page #\(page)")
         didShow?(page)
